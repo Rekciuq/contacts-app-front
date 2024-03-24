07f36660ee5a7c73aef3a57ef8cd94b981c676b7
Danylo <danilaka76@outlook.com>
2024-03-24 16:27:29 +0200

    Main html structure created.

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "Header",
+}
+</script>
+
+<template>
+  <a href="localhost:5173/">Contacts</a>
+</template>
+
+<style scoped>
+a {
+  color: #5f6368;
+  font-size: 2.5rem;
+  text-decoration-line: none;
+}
+</style>
